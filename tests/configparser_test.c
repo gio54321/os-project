db585f9f3175b4f5f8d661e982e7e3aa23ec896d
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-05-07 11:10:17 +0200

    Add configparser test

--- tests/configparser_test.c
+++ tests/configparser_test.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "configparser.h"
 
 int main(void)
 {
-    printf("hello world!\n");
+    struct config_t* config;
+    config = get_config_from_file("tests/example_config.txt");
+
+    char *key, *value;
+    int res;
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 1 || strcmp(key, "A") != 0 || strcmp(value, "A") != 0)
+        return -1;
+
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 1 || strcmp(key, "B") != 0 || strcmp(value, "30") != 0)
+        return -1;
+
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 1 || strcmp(key, "C") != 0 || strcmp(value, "aaa") != 0)
+        return -1;
+
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 1 || strcmp(key, "vvv") != 0 || strcmp(value, "aaa") != 0)
+        return -1;
+
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 0)
+        return -1;
+    res = config_get_next_entry(config, &key, &value);
+    if (res != 0)
+        return -1;
+
+    destroy_config(config);
     return 0;
 }
