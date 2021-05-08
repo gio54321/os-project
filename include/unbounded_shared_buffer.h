9f48c9f9c372b55771c0298ab60860ed9fcd732c
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-05-08 13:30:04 +0200

    Add usbuf documentation and comments

--- include/unbounded_shared_buffer.h
+++ include/unbounded_shared_buffer.h
@@ -7,9 +7,40 @@ typedef enum {
     LIFO_POLICY
 } buf_policy;
 
+// Create an unbounded shared buffer with given policy.
+// The function returns NULL on error
+// (e.g. policy is neither FIFO_POLICY or LIFO_POLICY, allocation error etc.)
+// otherwise it returns a pointer to a valid usbuf_t.
+// The buffer will need to be freed with usbuf_free.
+// The buffer holds void pointers, to make it generic.
+// The user will need to take care of allocation/deallocation of such pointers.
+// usbuf_t shall be treated as opaque, so it shall be only accessed via the library
+// functions.
 usbuf_t* usbuf_create(buf_policy policy);
+
+// Put an element in the shared buffer.
+// This operation is always non-blocking.
+// The function returns 0 on successful insertion, otherwise it returns -1.
+// if the function returns -1 something very bad happened (e.g. the internal
+// mutex failed to lock/unlock) and the buffer shall be considered corrupted.
+// any operation after -1 is returned shall be considered U.B.
 int usbuf_put(usbuf_t* buf, void* item);
+
+// Put an element in the shared buffer.
+// If the buffer is empty, then the calls blocks until there is an element available.
+// The function returns the pointer of the item on successful extraction,
+// otherwise it returns NULL.
+// if the function returns NULL something very bad happened (e.g. the internal
+// mutex failed to lock/unlock) and the buffer shall be considered corrupted.
+// any operation after NULL is returned shall be considered U.B.
 void* usbuf_get(usbuf_t* buf);
+
+// free the shared buffer buf
+// if the buffer is non-empty, then the function returns -1 and the memory is not freed,
+// since the library doesn't know what to do with the void pointesr stored inside
+// if the buffer is empty, then the memory is freed.
+// any operation performed after 0 is returned shall be considered U.B.
+// this operation shall not be done when the buffer is still shared between threads
 int usbuf_free(usbuf_t* buf);
 
 #endif
--- src/unbounded_shared_buffer.c
+++ src/unbounded_shared_buffer.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 #include "unbounded_shared_buffer.h"
-#define INITIAL_CAPACITY 4
 
 struct node {
     void* data;
@@ -18,17 +17,29 @@ struct usbuf_s {
     pthread_cond_t not_anymore_empty;
 };
 
+// Create an unbounded shared buffer with given policy.
+// The function returns NULL on error
+// (e.g. policy is neither FIFO_POLICY or LIFO_POLICY, allocation error etc.)
+// otherwise it returns a pointer to a valid usbuf_t.
+// The buffer will need to be freed with usbuf_free.
+// The buffer holds void pointers, to make it generic.
+// The user will need to take care of allocation/deallocation of such pointers.
+// usbuf_t shall be treated as opaque, so it shall be only accessed via the library
+// functions.
 usbuf_t* usbuf_create(buf_policy policy)
 {
+    // policy has to be FIFO_POLICY or LIFO_POLICY
     if (policy != FIFO_POLICY && policy != LIFO_POLICY) {
         return NULL;
     }
 
+    // allocate the new buffer
     usbuf_t* buf = malloc(sizeof(usbuf_t));
     if (buf == NULL) {
         return NULL;
     }
 
+    // initializa all the fields of the struct
     buf->policy = policy;
     buf->start = NULL;
     buf->end = NULL;
@@ -44,9 +55,17 @@ usbuf_t* usbuf_create(buf_policy policy)
         free(buf);
         return NULL;
     }
+
+    //finally return the pointer to the buffer to the caller
     return buf;
 }
 
+// Put an element in the shared buffer.
+// This operation is always non-blocking.
+// The function returns 0 on successful insertion, otherwise it returns -1.
+// if the function returns -1 something very bad happened (e.g. the internal
+// mutex failed to lock/unlock) and the buffer shall be considered corrupted.
+// any operation after -1 is returned shall be considered U.B.
 int usbuf_put(usbuf_t* buf, void* item)
 {
     int lock_res = pthread_mutex_lock(&buf->mutex);
@@ -54,14 +73,20 @@ int usbuf_put(usbuf_t* buf, void* item)
         return -1;
     }
 
+    // create the new node
     struct node* new_node = malloc(sizeof(struct node));
     if (new_node == NULL) {
         return -1;
     }
 
+    // put the data in the newly created node
     new_node->data = item;
 
+    // if the policy is FIFO, then the node shall be inserted at the end of the
+    // linked list, otherwise the node shall be inserted at the start of the
+    // linked list
     if (buf->policy == FIFO_POLICY) {
+        // insert node at the end of the linked list
         new_node->next = NULL;
         if (buf->end == NULL) {
             buf->start = new_node;
@@ -80,6 +105,8 @@ int usbuf_put(usbuf_t* buf, void* item)
         }
     }
 
+    // signal to one threads that was waiting for the buffer to become
+    // non-empty
     int signal_res = pthread_cond_signal(&buf->not_anymore_empty);
     if (signal_res != 0) {
         return -1;
@@ -89,9 +116,17 @@ int usbuf_put(usbuf_t* buf, void* item)
     if (unlock_res != 0) {
         return -1;
     }
+
     return 0;
 }
 
+// Put an element in the shared buffer.
+// If the buffer is empty, then the calls blocks until there is an element available.
+// The function returns the pointer of the item on successful extraction,
+// otherwise it returns NULL.
+// if the function returns NULL something very bad happened (e.g. the internal
+// mutex failed to lock/unlock) and the buffer shall be considered corrupted.
+// any operation after NULL is returned shall be considered U.B.
 void* usbuf_get(usbuf_t* buf)
 {
     int lock_res = pthread_mutex_lock(&buf->mutex);
@@ -99,12 +134,15 @@ void* usbuf_get(usbuf_t* buf)
         return NULL;
     }
 
+    // wait for the buffer to become non-empty
     while (buf->start == NULL) {
         int wait_res = pthread_cond_wait(&buf->not_anymore_empty, &buf->mutex);
         if (wait_res != 0) {
             return NULL;
         }
     }
+
+    // remove the stating node
     void* res = buf->start->data;
     struct node* to_be_removed_node = buf->start;
     buf->start = buf->start->next;
@@ -112,13 +150,21 @@ void* usbuf_get(usbuf_t* buf)
         buf->end = NULL;
     }
     free(to_be_removed_node);
+
     int unlock_res = pthread_mutex_unlock(&buf->mutex);
     if (unlock_res != 0) {
         return NULL;
     }
+
     return res;
 }
 
+// free the shared buffer buf
+// if the buffer is non-empty, then the function returns -1 and the memory is not freed,
+// since the library doesn't know what to do with the void pointesr stored inside
+// if the buffer is empty, then the memory is freed.
+// any operation performed after 0 is returned shall be considered U.B.
+// this operation shall not be done when the buffer is still shared between threads
 int usbuf_free(usbuf_t* buf)
 {
     if (buf->start != NULL) {
