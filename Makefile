93cf84ad64c01956de0501909a68d50d3a09252a
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-05-21 16:46:36 +0200

    Separate memory leak tests from concurrency tests

--- Makefile
+++ Makefile
@@ -9,19 +9,26 @@ CFLAGS = -Wall -pedantic -std=gnu17 \
 		-I$(IDIR) -g 
 LIBS = -lpthread
 
-_OBJ = configparser unbounded_shared_buffer protocol int_queue
+_OBJ = configparser unbounded_shared_buffer protocol int_queue file_storage_internal
+CONCURRENT_OBJ = unbounded_shared_buffer
+
 OBJ = $(patsubst %,$(OBJDIR)/%.o,$(_OBJ))
 TESTS = $(patsubst %,$(BINDIR)/%_test,$(_OBJ))
 RUNTESTS = $(patsubst %,run_%_test,$(_OBJ))
 
-.PHONY: all tests run-tests clean
+CTESTS = $(patsubst %,$(BINDIR)/%_ctest,$(CONCURRENT_OBJ))
+RUNCTESTS = $(patsubst %,run_%_ctest,$(CONCURRENT_OBJ))
+
+.PHONY: all tests run-all-tests run-tests run-ctests clean
 
 all: $(OBJ)
 
 clean:
 	rm -f $(OBJ) $(TESTS)
 
+run-all-tests: run-tests run-ctests
 run-tests: tests $(RUNTESTS)
+run-ctests: tests $(RUNCTESTS)
 tests: $(TESTS)
 
 $(OBJDIR)/configparser.o: $(SRCDIR)/configparser.c $(IDIR)/configparser.h
@@ -36,13 +43,25 @@ $(OBJDIR)/protocol.o: $(SRCDIR)/protocol.c $(IDIR)/protocol.h
 $(OBJDIR)/int_queue.o: $(SRCDIR)/int_queue.c $(IDIR)/int_queue.h
 	$(CC) $(CFLAGS) -c $< -o $@
 
+$(OBJDIR)/file_storage_internal.o: $(SRCDIR)/file_storage_internal.c $(IDIR)/file_storage_internal.h $(OBJDIR)/int_queue.o
+	$(CC) $(CFLAGS) -c $< -o $@ $(LIBS)
+
 # generic rule for all tests
-$(TESTS): $(BINDIR)/%_test: $(TESTDIR)/%_test.c $(OBJDIR)/%.o
+$(TESTS): $(BINDIR)/%_test: $(TESTDIR)/%_test.c $(OBJ)
 	$(CC) $(CFLAGS) $^ -o $@ $(LIBS)
 
+$(CTESTS): $(BINDIR)/%_ctest: $(TESTDIR)/%_ctest.c $(OBJ)
+	$(CC) $(CFLAGS) $^ -o $@ $(LIBS)
 
 # the test fails if the test process returns a nonzero value or valgrind detects any leak
 $(RUNTESTS): run_%_test: $(BINDIR)/%_test
+	@tput setaf 7
 	@echo "--> running test $<"
 	@(valgrind --quiet --leak-check=full --error-exitcode=1 $< && (tput setaf 2; echo "TEST PASSED")) || (tput setaf 1; echo "TEST FAILED")
+	@tput setaf 7
+
+$(RUNCTESTS): run_%_ctest: $(BINDIR)/%_ctest
+	@tput setaf 7
+	@echo "--> running concurrency test $<"
+	@($< && (tput setaf 2; echo "TEST PASSED")) || (tput setaf 1; echo "TEST FAILED")
 	@tput setaf 7
--- tests/unbounded_shared_buffer_test.c
+++ tests/unbounded_shared_buffer_test.c
@@ -6,64 +6,6 @@
 
 #include "unbounded_shared_buffer.h"
 
-#define DO_CONCURRENCY_TESTS 0
-
-unsigned int num_items;
-usbuf_t* buf;
-
-void* producer(void* arg)
-{
-    unsigned int rand_state = time(NULL);
-    int t = rand_r(&rand_state);
-    for (int i = 0; i < num_items; ++i) {
-        assert(usbuf_put(buf, &num_items) == 0);
-        usleep(((double)t / RAND_MAX) * 1000);
-    }
-    return NULL;
-}
-
-void* consumer(void* arg)
-{
-    unsigned int rand_state = time(NULL);
-    int t = rand_r(&rand_state);
-    for (;;) {
-        void* res = NULL;
-        int get_res = usbuf_get(buf, &res);
-        assert(get_res == 0 || get_res == -2);
-        if (get_res == -2) {
-            // buffer is closed, return
-            break;
-        }
-        assert(res == &num_items);
-        usleep(((double)t / RAND_MAX) * 1000);
-    }
-    return NULL;
-}
-
-void concurrent_test(unsigned int num_producers, unsigned int num_consumers, unsigned int num, buf_policy policy)
-{
-    num_items = num;
-    buf = usbuf_create(policy);
-    pthread_t producers_tids[num_producers];
-    pthread_t consumers_tids[num_consumers];
-    for (int i = 0; i < num_producers; ++i) {
-        pthread_create(&producers_tids[i], NULL, producer, NULL);
-    }
-    for (int i = 0; i < num_consumers; ++i) {
-        pthread_create(&consumers_tids[i], NULL, consumer, NULL);
-    }
-    for (int i = 0; i < num_producers; ++i) {
-        pthread_join(producers_tids[i], NULL);
-    }
-    // since all the producers finished, close the buffer and wait
-    // for all the consumers
-    assert(usbuf_close(buf) == 0);
-    for (int i = 0; i < num_consumers; ++i) {
-        pthread_join(consumers_tids[i], NULL);
-    }
-    assert(usbuf_free(buf) == 0);
-}
-
 int main(void)
 {
     // tests basic policy properties
@@ -100,20 +42,5 @@ int main(void)
     assert(res == &a);
     usbuf_free(local_buf);
 
-    if (DO_CONCURRENCY_TESTS) {
-        printf("running concurrency tests (this may take a while)...\n");
-        concurrent_test(1, 1, 50, LIFO_POLICY);
-        concurrent_test(2, 2, 50, LIFO_POLICY);
-        concurrent_test(5, 5, 50, LIFO_POLICY);
-        concurrent_test(30, 30, 50, LIFO_POLICY);
-        concurrent_test(30, 5, 50, LIFO_POLICY);
-        concurrent_test(5, 30, 50, LIFO_POLICY);
-
-        concurrent_test(1, 1, 50, FIFO_POLICY);
-        concurrent_test(2, 2, 50, FIFO_POLICY);
-        concurrent_test(5, 5, 50, FIFO_POLICY);
-        concurrent_test(30, 30, 50, FIFO_POLICY);
-        concurrent_test(30, 5, 50, FIFO_POLICY);
-        concurrent_test(5, 30, 50, FIFO_POLICY);
-    }
+    return 0;
 }
