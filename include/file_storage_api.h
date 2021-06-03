c9990194aba4ec08e5acbf4c5af411a73b2491e8
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-06-03 11:16:49 +0200

    Convert API prototypes to camelcase

--- include/file_storage_api.h
+++ include/file_storage_api.h
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 
-int open_connection(const char* sockname, int msec, const struct timespec abstime);
-int close_connection(const char* sockname);
+int openConnection(const char* sockname, int msec, const struct timespec abstime);
+int closeConnection(const char* sockname);
 
-int open_file(const char* pathname, int flags);
-int read_file(const char* pathname, void** buf, size_t* size);
-int write_file(const char* pathname, const char* dirname);
-int append_to_file(const char* pathname, void* buf, size_t size, const char* dirname);
+int openFile(const char* pathname, int flags);
+int readFile(const char* pathname, void** buf, size_t* size);
+int writeFile(const char* pathname, const char* dirname);
+int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname);
 
-int lock_file(const char* pathname);
-int unlock_file(const char* pathname);
+int lockFile(const char* pathname);
+int unlockFile(const char* pathname);
 
-int close_file(const char* pathname);
-int remove_file(const char* pathname);
+int closeFile(const char* pathname);
+int removeFile(const char* pathname);
