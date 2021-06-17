54546d4d5abf44f2b75286fa64b96bfb31c0ce5e
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-06-17 12:19:05 +0200

    Add initial implementation of OPEN_FILE

--- include/server_worker.h
+++ include/server_worker.h
@@ -9,11 +9,13 @@
 */
 typedef struct worker_arg_s {
     usbuf_t* master_to_workers_buffer;
+    usbuf_t* logger_buffer;
     int worker_to_master_pipe_write_fd;
     file_storage_t* file_storage;
 
-    unsigned long max_num_files;
-    unsigned long max_storage_size;
+    long max_num_files;
+    long max_storage_size;
+    bool enable_compression;
 } worker_arg_t;
 
 void* server_worker_entry_point(void* arg);
--- src/server_worker.c
+++ src/server_worker.c
@@ -1,7 +1,9 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "file_storage_internal.h"
+#include "logger.h"
 #include "protocol.h"
 #include "server_worker.h"
 #include "thread_pool.h"
@@ -13,15 +15,28 @@ static int client_cleanup(file_storage_t* file_storage, int client_fd)
     return 0;
 }
 
+static int send_error(int client_fd, char err_code)
+{
+    struct packet err_packet;
+    clear_packet(&err_packet);
+
+    err_packet.op = ERROR;
+    err_packet.err_code = err_code;
+    return send_packet(client_fd, &err_packet);
+}
+
 static void server_worker(unsigned int num_worker, worker_arg_t* worker_args)
 {
     usbuf_t* master_to_workers_buf = worker_args->master_to_workers_buffer;
+    usbuf_t* logger_buffer = worker_args->logger_buffer;
     file_storage_t* file_storage = worker_args->file_storage;
     int worker_to_master_pipe = worker_args->worker_to_master_pipe_write_fd;
-    unsigned int max_num_files = worker_args->max_num_files;
-    unsigned int max_storage_size = worker_args->max_storage_size;
+    long max_num_files = worker_args->max_num_files;
+    long max_storage_size = worker_args->max_storage_size;
 
     rw_lock_t* storage_lock = get_rw_lock_from_storage(file_storage);
+
+    LOG(logger_buffer, "Worker #%d started", num_worker);
 
     for (;;) {
         void* client_fd_ptr;
@@ -39,8 +54,9 @@ static void server_worker(unsigned int num_worker, worker_arg_t* worker_args)
         clear_packet(&client_packet);
 
         // receive the client request
-        int receive_res = receive_packet(client_fd, &client_packet);
-        if (receive_res < 0) {
+        int receive_res;
+        DIE_NEG1(receive_res = receive_packet(client_fd, &client_packet), "receive_packet");
+        if (receive_res == 0) {
             // the client disconnected
             perror("receive packet");
 
@@ -59,6 +75,50 @@ static void server_worker(unsigned int num_worker, worker_arg_t* worker_args)
 
         switch (client_packet.op) {
         case OPEN_FILE:
+            LOG(logger_buffer, "client %d requested to open %s", client_fd, client_packet.filename);
+            write_lock(storage_lock);
+            vfile_t* file_to_open = get_file_from_name(file_storage, client_packet.name_length, client_packet.filename);
+            if (file_to_open == NULL) {
+                if (errno = ENOENT) {
+                    // file does not exists in the storage
+                    // if the flag O_CREATE is set then create the file, else send
+                    // an error to the client
+                    if (client_packet.flags & O_CREATE) {
+                        LOG(logger_buffer, "creating file %s", client_packet.filename);
+                        DIE_NULL(file_to_open = create_vfile(), "create vfile");
+                        file_to_open->filename = client_packet.filename;
+                        client_packet.filename = NULL;
+                        DIE_NEG1(add_vfile_to_storage(file_storage, file_to_open), "add file to storage");
+                    } else {
+                        LOG(logger_buffer, "file open requested for %s but the file does not exist in the storage", client_packet.filename);
+                        DIE_NEG1(send_error(client_fd, FILE_DOES_NOT_EXIST), "send error");
+                    }
+                } else {
+                    perror("get file from name");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // se the client fd in the opened by set
+            FD_SET(client_fd, &file_to_open->opened_by);
+
+            // if the flag O_LOCK is set then try to lock the file
+            if (client_packet.flags & O_LOCK) {
+                if (file_to_open->locked_by == -1) {
+                    LOG(logger_buffer, "client %d locked the file %s", client_fd, client_packet.filename);
+                    file_to_open->locked_by = client_fd;
+                } else {
+                    LOG(logger_buffer, "client %d requested to lock the file %s but was already locked, operation failed", client_fd, client_packet.filename);
+                    // if the file is already locked then fail
+                    DIE_NEG1(send_error(client_fd, FILE_ALREADY_LOCKED), "send error");
+
+                    // clear the client in the opened by set (the opertion failed, so
+                    // the file is not opened by the client)
+                    FD_CLR(client_fd, &file_to_open->opened_by);
+                }
+            }
+
+            write_unlock(storage_lock);
             break;
         case READ_FILE:
             break;
