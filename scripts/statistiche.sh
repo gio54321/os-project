90f3ff92b4b6f17db3e9043761e723227ec1f1f0
Giorgio Dell'Immagine <g.dellimmagine@gmail.com>
2021-07-10 16:39:52 +0200

    Add documentation to statistics script

--- scripts/statistiche.sh
+++ scripts/statistiche.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# check if the user provided an argument to the script
 if [ $# -ne 1 ]
   then
     echo "usage: $0 logfile"
     exit 1
 fi
 
+# check if the argument is a file
 if ! [ -e $1 ]
   then
     echo "$1 is not a file"
     exit 1
 fi
 
+# calculate the requests and the successes of each operation
 for op in open close write read read_n append lock unlock remove
 do
   n_req=$(grep -o ".${op}. REQUEST" log.txt | wc -l)
@@ -19,12 +22,15 @@ do
   echo "Number of $op operations {requests:$n_req, successes:$n_succ}"
 done
 
+# calculate the number of replacements occurred
 n_rep=$(grep -o "REPLACEMENT" log.txt | wc -l)
 echo "Number of times the replacement algorithm ran $n_rep"
 
+# calculate the number of opens with the flag O_LOCK set to 1
 n=$(grep -o "lock:1" log.txt | wc -l)
 echo "Number of open-lock requests: $n"
 
+# calculate the avergae write size
 written=$(grep -o "written_bytes:[0-9]*" log.txt | awk -F ':' '{print $2}')
 if [ -z "$written" ]
 then
@@ -33,9 +39,9 @@ fi
 written_sum=$( echo $written | sed -r 's/ /+/g' | bc)
 written_len=$(echo $written | wc -w)
 written_avg=$(echo "scale=2;$written_sum/$written_len" | bc)
-
 echo "Average size of write operations: $written_avg byte"
 
+# calculate the avergae read size
 read=$(grep -o "sent_bytes:[0-9]*" log.txt | awk -F ':' '{print $2}')
 if [ -z "$read" ]
 then
@@ -44,10 +50,9 @@ fi
 read_sum=$( echo $read | sed -r 's/ /+/g' | bc)
 read_len=$(echo $read | wc -w)
 read_avg=$(echo "scale=2;$read_sum/$read_len" | bc)
-
-
 echo "Average size of read operations:  $read_avg byte"
 
+# show the number of requests served by each worker
 echo ""
 echo "REQUESTS SERVED PER WORKER"
 grep -o "worker [0-9]* requests served: [0-9]*" log.txt | sort
